ea3a75c6329fa9e8b44e10cd94809844eb864131
jpancorb <jpancorb@taniwa.es>
2024-10-14 14:48:26 +0200

    username

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,8 +5,7 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
-  name: 'auth',
   loginWith: {
-    email: true
+    // email: true
   },
 });
